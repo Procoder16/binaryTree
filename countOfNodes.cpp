6f0ef13f974574fc7670991fc267d807598917be
Procoder16 <smsoumik1608@gmail.com>
2021-06-26 02:06:29 +0530

    program to count the number of nodes in a binary tree added

---
+++ countOfNodes.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int countOfNodes(Node* root){
+    if(root ==  NULL){
+        return 0;
+    }
+    return countOfNodes(root -> left) + countOfNodes(root -> right) + 1;
+}
+
+int main(){
+
+    struct Node* root = new Node(1);
+
+    root -> left = new Node(2);
+    root -> right = new Node(3);
+
+    root -> left -> left = new Node(4);
+    root -> left -> right = new Node(5);
+
+    root -> right -> left = new Node(6);
+    root -> right -> right = new Node(7);
+
+    /*
+                1
+              /   \
+             2     3
+            / \   / \
+           4   5 6   7
+    */
+
+    cout<<countOfNodes(root);
+
+    return 0;
+}
