6d9cc526a05b177d1f5a6e1d0918e73be463bc47
Procoder16 <smsoumik1608@gmail.com>
2021-07-04 12:48:50 +0530

    program to search for a node in BST added

---
+++ searchInBST.cpp
@@ -0,0 +1,113 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+/*
+     
+*/
+
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int d){
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void bfs(Node* root){
+    if(root == NULL){
+        return;
+    }
+
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+        if(temp != NULL){
+            cout<<temp -> data<<" ";
+            if(temp -> left){
+                q.push(temp -> left);
+            }
+            if(temp -> right){
+                q.push(temp -> right);
+            }
+        }
+        else{
+            cout<<endl;
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+    }
+}
+
+Node* insertInBST(Node* root, int d){
+    if(root == NULL){
+        return new Node(d);
+    }
+
+    if(d <= root -> data){
+        root -> left = insertInBST(root -> left, d);
+    }
+    else{
+        root -> right = insertInBST(root -> right, d);
+    }
+
+    return root;
+}
+
+Node* build(){
+    int d;
+    cin>>d;
+
+    Node* root = NULL;
+
+    while(d!=-1){
+        root = insertInBST(root, d);
+        cin>>d;
+    }
+    return root; 
+}
+
+bool searchInBST(Node* root, int t){
+    if(root == NULL){
+        return false;
+    }
+    if(root -> data == t){
+        return true;
+    }
+
+    if(t <= root -> data){
+        return searchInBST(root -> left, t);
+    }
+    else{
+        return searchInBST(root -> right, t);
+    }
+}
+
+int main(){
+    Node* root = build();
+    cout<<"THE TREE LOOKS LIKE THIS:"<<endl;
+    bfs(root);
+    cout<<endl;
+    int t;
+    cout<<"ENTER THE NODE TO SEARCH:";
+    cin>>t;
+    cout<<endl;
+    if(searchInBST(root, t)){
+        cout<<"NODE FOUND!!!";
+    }
+    else{
+        cout<<"NODE NOT PRESENT!!!";
+    }
+
+    return 0;
+}
