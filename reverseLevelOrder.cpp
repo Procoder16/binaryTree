7e4e443372d0f2ca45e3c04316e9b9ffef206b8f
Procoder16 <smsoumik1608@gmail.com>
2021-07-15 00:16:13 +0530

    added the documentation

--- reverseLevelOrder.cpp
+++ reverseLevelOrder.cpp
@@ -1,6 +1,14 @@
 #include<bits/stdc++.h>
-
 using namespace std;
+
+/*
+    The approach is pretty similar to bfs printing only but with a little variation:
+        >> Here, we will be using a queue as we did in the BFS printing, also we would be using a stack since the reverse is required.
+        >> While adding the nodes, in case of BFS, we added first the left child and then the right child. But, here we would push the
+           right child and then the left child to the queue.
+        >> And for each node we would be pushing it to the stack also even before checking for the children. We would add the stack part
+           when the top element of the queue is deleted.[This is just fir remembrance and not the exact logic]     
+*/
 
 class Node{
     public:
