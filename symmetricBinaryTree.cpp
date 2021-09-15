0714f4583678947af4879fe03b48c5f2087316b8
Procoder16 <smsoumik1608@gmail.com>
2021-09-15 11:42:20 +0530

    program to check for symmetric binary tree added

---
+++ symmetricBinaryTree.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+bool isSymmetricHelp(Node* left, Node* right){
+    if(left == NULL || right == NULL){
+        return left == right;
+    }
+    if(left -> data != right -> data){
+        return false;
+    }
+    return (isSymmetricHelp(left -> left, right -> right) && isSymmetricHelp(left -> right, right -> left));
+}
+
+bool isSymmetric(Node* root){
+    return (root == NULL || isSymmetricHelp(root -> left, root -> right));
+}
+
+int main(){
+
+    Node* root = new Node(1);
+    root -> left = new Node(2);
+    root -> left = new Node(2);
+
+    root -> left -> left = new Node(4);
+    root -> right -> right = new Node(4);
+
+    root -> left -> right = new Node(3);
+    root -> right -> left = new Node(3);
+
+    if(isSymmetric(root)){
+        cout<<"THE GIVEN BINARY TREE IS SYMMETRIC!";
+    }
+    else{
+        cout<<"THE GIVEN BINARY TREE IS ASYMMETRIC!";
+    } 
+
+    return 0;
+}
