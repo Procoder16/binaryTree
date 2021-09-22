a49d56e40ca8ecdde041723188b72bf9562eb6e2
Procoder16 <smsoumik1608@gmail.com>
2021-09-22 22:29:10 +0530

    main modication function added

--- childrenSumProperty.cpp
+++ childrenSumProperty.cpp
@@ -44,6 +44,49 @@ void modifiedBFS(Node* root){
     }
 }
 
+void childSumPropertyModification(Node* root){
+
+    if(root == NULL){
+        return;
+    }
+
+    int child = 0;
+    if(root -> left){
+        child += root -> left -> data;
+    }
+    if(root -> right){
+        child += root -> right -> data;
+    }
+
+    if(child >= root -> data){
+        root -> data = child;
+    }
+
+    else{
+        if(root -> left){
+            root -> left -> data = root -> data;
+        }
+        if(root -> right){
+            root -> right -> data  = root -> data;
+        }
+    }
+
+    childSumPropertyModification(root -> left);
+    childSumPropertyModification(root -> right);
+
+    int total = 0;
+    if(root -> left){
+        total += root -> left -> data;
+    }
+    if(root -> right){
+        total += root -> right -> data;
+    }
+
+    if(root -> left || root -> right){
+        root -> data = total;
+    }
+}
+
 int main(){
 
     Node* root = new Node(7);
@@ -55,6 +98,14 @@ int main(){
     root -> left -> right = new Node(3);
     root -> right -> left = new Node(2);
     root -> right -> right = new Node(1);
+
+    cout<<"TREE WITHOUT MODIFICATION:\n";
+
+    modifiedBFS(root);
+
+    childSumPropertyModification(root);
+
+    cout<<"TREE AFTER MODIFICATION:\n";
 
     modifiedBFS(root);
 
