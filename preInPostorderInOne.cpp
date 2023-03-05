3252ed021044d145e5e13106106598c60b2f3855
Procoder16 <smsoumik1608@gmail.com>
2023-03-05 23:47:34 +0530

    header files updated

--- preInPostorderInOne.cpp
+++ preInPostorderInOne.cpp
@@ -89,3 +89,40 @@ int main(){
 
     return 0;
 }
+
+int main() {
+   // Declaration of a static array with size 5
+   int arr[5];
+
+   // Assigning values to the array
+   arr[0] = 10;
+   arr[1] = 20;
+   arr[2] = 30;
+   arr[3] = 40;
+   arr[4] = 50;
+
+   // Accessing the elements of the array
+   for (int i = 0; i < 5; i++) {
+      std::cout << arr[i] << " ";
+   }
+   return 0;
+}
+
+public class Main {
+   public static void main(String[] args) {
+      // Declaration of a static array with size 5
+      int[] arr = new int[5];
+
+      // Assigning values to the array
+      arr[0] = 10;
+      arr[1] = 20;
+      arr[2] = 30;
+      arr[3] = 40;
+      arr[4] = 50;
+
+      // Accessing the elements of the array
+      for (int i = 0; i < 5; i++) {
+         System.out.print(arr[i] + " ");
+      }
+   }
+}
